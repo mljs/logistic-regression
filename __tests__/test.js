c175f82aa40a2b6527cf791410320d3036aa37f2
jajoe <joseph.lefevre@insa-rouen.fr>
2017-07-21 15:19:07 +0200

    fix: modify number of steps of the tests (and add the second test)

--- __tests__/test.js
+++ __tests__/test.js
@@ -18,4 +18,17 @@ describe('Logistic Regression algorithm', function () {
         var results = logreg.predict(Xtest); // compute results of the training set
         expect(results).toEqual([0, 0, 0, 1, 1, 1]);
     });
+
+    it('Test of the prediction with 3 classes', function(){
+        var X = new Matrix([[0,-1], [1,0], [1,1], [1,-1], [2,0], [2,1], [2,-1], [3,2], [0,4], [1,3], [1,4], [1,5], [2,3], [2,4], [2,5], [3,4], [1, 10], [1, 12], [2, 10], [2,11], [2, 14], [3, 11]]);
+        var Y = Matrix.columnVector([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2]);
+
+        var Xtest = new Matrix([[0, -2], [1, 0.5], [1.5, -1], [1, 2.5], [2, 3.5], [1.5, 4], [1, 10.5], [2.5, 10.5], [2, 11.5]])
+        var Ytest = Matrix.columnVector([0, 0, 0, 1, 1, 1, 2, 2, 2]);
+
+        var logreg = new LogisticRegression({numSteps: 1000, learningRate: 5e-3});
+        logreg.train(X,Y);
+        var finalResults = logreg.predict(Xtest);
+        expect(finalResults).toEqual([0, 0, 0, 1, 1, 1, 2, 2, 2]);
+    });
 });
